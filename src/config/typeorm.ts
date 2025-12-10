be10c7664a80ea5f6379964dd8b3a3f08af46d6a
Harshboghra <harshboghara01@gmail.com>
2025-12-10 19:55:44 +0530

    feat(config): add SSL configuration for production environment in TypeORM setup

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -18,5 +18,9 @@ export const typeOrmConfig = (): DataSourceOptions => {
     migrations: [join(__dirname, '..', '**', '/migrations/*.{ts,js}')],
     synchronize: false,
     migrationsTableName: 'migrations',
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
   };
 };
